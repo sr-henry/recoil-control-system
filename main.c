d71b7acc71a63edc6066cc733e628621c19474f4
sr-henry <37659078+sr-henry@users.noreply.github.com>
2018-11-15 18:00:17 -0200

    testes

--- main.c
+++ main.c
@@ -10,38 +10,56 @@
 #define True 1
 #define Time 105
 
+typedef struct $
+{
+    int nBullets;
+    int spray[2][30];
+    int time;   
+}weapon;
+
 int main()
 {
     POINT mouse;
 
-    int cX, cY, bx, by;
+    int cX, cY;
     int i = 0;
 
-        int x[] = {0,0,0,-2,-4,-6,-6,1,1,18,16,11,10,6,-2,-14,-20,-19,-19,-12,-4,2,2,-1,-2,1,14,22,26,27};
-        int y[] = {0,-19,-16,-22,-24,-26,-22,-16,-9,-5,-3,-4,-2,0,-1,-4,-4,-3,0,1,2,-1,-4,-4,-2,-1,2,5,8,6};
+    weapon ak;
+
+    ak.nBullets = 30;
+    ak.time = 105;
+
+    FILE *cfg = fopen("config.txt", "r");
+    for (i = 0; i < 30; ++i)
+    {
+        fscanf(cfg,"%d", &ak.spray[0][i]);
+    }
+
+    for (i = 0; i < 30; ++i)
+    {
+        fscanf(cfg,"%d", &ak.spray[1][i]);
+    }  
+
+    fclose(cfg);
 
     puts("--Insert--");
 
     while(True)
     {
-        if (GetKeyState(VK_INSERT))
+        while (GetKeyState(VK_INSERT))
         {
-            GetCursorPos(&mouse);
-            bx = mouse.x;
-            by = mouse.y;
             while (GetAsyncKeyState(VK_LBUTTON))
             {
                 GetCursorPos(&mouse);
-                cX = mouse.x - x[i];
-                cY = mouse.y - y[i];
+                cX = mouse.x - ak.spray[0][i];
+                cY = mouse.y - ak.spray[1][i];
                 SetCursorPos(cX, cY);
                 Sleep(Time);
                 i++;
                 if (i == 30){ break; }
             }
-            SetCursorPos(bx, by);
             i = 0;
-        }   
+        }  
     }
     return 0;
 }
