7d3159ba0ca027ec58098ea824aada3a29ad811e
sr-henry <37659078+sr-henry@users.noreply.github.com>
2018-11-17 12:40:44 -0200

    +ump45
    +Nova Arma (ump45);
    +Modularização;

--- src/spraybom.c
+++ src/spraybom.c
@@ -5,32 +5,43 @@
 #include <stdio.h>
 
 #define True 1
-#define Time 40
 
-int main()
+int macro(int cSpray[2][30], int nBullets, int tSpray)
 {
-    int i = 0, fx, fy;
+	POINT mouse;
+	int i = 0;
 
-    int spray[2][30] = {{0,0,0,-2,-4,-6,-6,1,1,18,16,11,10,6,-2,-14,-20,-19,-19,-12,-4,2,2,-1,-2,1,14,22,26,27},
-                        {0,-19,-16,-22,-24,-26,-22,-16,-9,-5,-3,-4,-2,0,-1,-4,-4,-3,0,1,2,-1,-4,-4,-2,-1,2,5,8,6}};
-
-    puts("--Insert--");
-
-    POINT mouse;
-
-    while (True)
+	while (True)
     { 
         while (GetAsyncKeyState(VK_LBUTTON) && GetKeyState(VK_INSERT))
         {
             GetCursorPos(&mouse);
-            SetCursorPos((mouse.x - spray[0][i]), (mouse.y - spray[1][i]));
-            Sleep(Time);
+            SetCursorPos((mouse.x - cSpray[0][i]), (mouse.y - cSpray[1][i]));
+            Sleep(tSpray);
             i++;
-            if (i == 30){break;}
+            if (i == nBullets){break;}
         }
         i = 0; 
     }
+
+	return 0;
+}
+
+
+int main()
+{
     
+
+    int ak47[2][30] = {{0,0,0,-2,-4,-6,-6,1,1,18,16,11,10,6,-2,-14,-20,-19,-19,-12,-4,2,2,-1,-2,1,14,22,26,27},
+                       {0,-19,-16,-22,-24,-26,-22,-16,-9,-5,-3,-4,-2,0,-1,-4,-4,-3,0,1,2,-1,-4,-4,-2,-1,2,5,8,6}};
+
+    int ump45[2][30] = {{0,-1,1,4,0,1,-3,-8,-11,-6,1,14,19,21,17,8,4,4,2,-7,-14,-17,-16,-12,-7,-1,-1,-2,-3,-3},
+                        {0,-9,-12,-13,-19,-19,-20,-16,-13,-7,-6,-3,0,0,2,-1,-2,0,0,-1,-3,-3,-1,-3,-1,-2,-2,-2,-2,0}};
+
+    puts("--Insert--");
+
+    macro(ak47, 30, 40);
+
     return 0;
 }
 
