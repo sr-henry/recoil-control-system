8f4f83f251d2bb36a4761b5a7903fcccfdbfe298
Henrique Campiotti <henrique260699@hotmail.com>
2018-12-24 13:10:20 -0200

    Update

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <windows.h>
 
 
+
 void trivela(int x, int y, int px, float t){
 	POINT mouse;
 	int i = 1;
@@ -20,11 +21,6 @@ int main ()
 {
 
 	puts("--Press Insert ON/OFF--");
-
-	POINT mouse;
-	int i = 0;
-
-	int x,y, d = 2;
  
 	while (1){
 
@@ -32,25 +28,13 @@ int main ()
 
 		while (GetAsyncKeyState(VK_LBUTTON) && GetKeyState(VK_INSERT)){
 
-			GetCursorPos(&mouse);
-
-			if (i == 4){ d--; }
-
-			SetCursorPos(mouse.x, mouse.y + d);
-			Sleep(1);
-
-			i++;
-
-			if (i == 500){
+				trivela( 0,	 1, 500, 1);
 				trivela(-3,	 1, 28, 17.85);
 				trivela( 5,  1, 30, 16.66);
 				trivela( 0,  1, 31, 16.12);
 				trivela(-4,	-1, 66, 7.57);
-			}	
-		}
 
-		i = 0; 
-		d = 2;
+		}
 	}
 
 	return 0;
