85633f7924908bacf0264ef4233c440f871f9816
Henrique Campiotti <henrique260699@hotmail.com>
2018-12-24 11:50:32 -0200

    Update
    +trivela

--- src/main.c
+++ src/main.c
@@ -2,7 +2,19 @@
 #include <string.h>
 #include <windows.h>
 
-#define T 1
+
+void trivela(int x, int y, int px, float t){
+	POINT mouse;
+	int i = 1;
+	while (GetAsyncKeyState(VK_LBUTTON) && GetKeyState(VK_INSERT)){
+		GetCursorPos(&mouse);
+		SetCursorPos(mouse.x + x, mouse.y + y);
+		Sleep(t);
+		i++;
+		if (i == px){break;}
+	}
+}
+
 
 int main () 
 {
@@ -22,9 +34,7 @@ int main ()
 
 			GetCursorPos(&mouse);
 
-			if (i == 4){
-				d--;
-			}
+			if (i == 4){ d--; }
 
 			SetCursorPos(mouse.x, mouse.y + d);
 			Sleep(1);
@@ -32,40 +42,11 @@ int main ()
 			i++;
 
 			if (i == 500){
-				printf("%d\n", i);
-				for (y = 0; y < 28; ++y){
-					GetCursorPos(&mouse);
-					SetCursorPos(mouse.x - 3, mouse.y + 1);
-					Sleep(17.85);
-					i++;
-				}printf("%d\n", i);
-
-				for (y = 0; y < 30; ++y){
-					GetCursorPos(&mouse);
-					SetCursorPos(mouse.x + 5, mouse.y + 1);
-					Sleep(16.66);
-					i++;
-				}printf("%d\n", i);
-
-				for (y = 0; y < 31; ++y){
-					GetCursorPos(&mouse);
-					SetCursorPos(mouse.x, mouse.y + 1);
-					Sleep(16.12);
-					i++;
-				}printf("%d\n", i);
-
-				for (y = 0; y < 66; ++y){
-					GetCursorPos(&mouse);
-					SetCursorPos(mouse.x - 4, mouse.y - 1);
-					Sleep(7.57);
-					i++;
-				}printf("%d\n", i);
-
-				puts("#####################");
-				Sleep(2000);
-				break;
-			}
-			//printf("%d\n", i);	
+				trivela(-3,	 1, 28, 17.85);
+				trivela( 5,  1, 30, 16.66);
+				trivela( 0,  1, 31, 16.12);
+				trivela(-4,	-1, 66, 7.57);
+			}	
 		}
 
 		i = 0; 
