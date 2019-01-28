ea0b447b008820c6711bd2ebb52eacd3b0c0b1b7
Henrique Campiotti <henrique260699@hotmail.com>
2019-01-28 16:41:16 -0200

    Update
    +arquivo de configuração externo
    ak-47
    m4a4

--- src/main.c
+++ src/main.c
@@ -1,40 +1,51 @@
 #include <windows.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include "config.h"
 
-#define AK 3000
-
-	void 	MouseMove		(int Dx, int Dy, int M, float T);
-	float 	CalculateDelay	(int gunTime, int len, float M);
+	void	ExecConfig		(int *dx, int *dy, int *M, int T, int len);
+	void	MouseMove		(int Dx, int Dy, int M, float T);
+	float	CalculateDelay	(int gunTime, int len, float M);
+	void	Menu			();
 
 int main()
-{
+{	
+	int op;
 
-	int dx[] = {0,1,1,1,1,-1,-1,-3,-5,-2,-4,-1,2,3,4,5,1,13,1,2,-1,-1,1,1,-6,-4,-2,-4};
-	int dy[] = {1,16,9,6,5,22,9,1,1,1,1,0,1,1,1,1,0,-1,-1,-1,2,5,4,3,-1,-1,-1,-1};
-	int  M[] = {100,4,8,13,13,2,2,12,8,11,5,21,2,12,11,8,40,2,5,2,4,2,2,3,5,13,25,17};
+	Menu();
+	scanf("%d", &op);
+	switch(op){
+		case 1:
+			ExecConfig(ak_dx, ak_dy, ak_M, AK47, ak_len);
+		break;
+		case 2:
+			ExecConfig(m4_dx, m4_dy, m4_M, M4A4, m4_len);
+		break;
+		default:
+			printf("Erro!\n");
+	}
+	
+	return 0;
+}
 
-	int len = sizeof(dx)/sizeof(dx[0]);
+void ExecConfig(int *dx, int *dy, int *M, int T, int len){
 
 	int i = 0;
 
 	while(true){
 		while(GetAsyncKeyState(VK_LBUTTON) && GetKeyState(VK_HOME)){
-			MouseMove(dx[i], dy[i], M[i], CalculateDelay(AK, len, M[i]));
+			MouseMove(dx[i], dy[i], M[i], CalculateDelay(T, len, M[i]));
 			i++;
 			if (i == len){break;}
 		}
 		i = 0;
 	}
-
-	return 0;
 }
-
 
 void MouseMove(int Dx, int Dy, int M, float T){
 	POINT mouse;
 	int count = 0;
-	while (GetAsyncKeyState(VK_LBUTTON)){
+	while(GetAsyncKeyState(VK_LBUTTON)){
 		GetCursorPos(&mouse);
 		SetCursorPos(mouse.x + Dx, mouse.y + Dy);
 		Sleep(T);
@@ -48,4 +59,12 @@ float CalculateDelay(int gunTime, int len, float M){
 	return result;
 }
 
-
+void Menu(){
+	printf("___EXTERNAL_RCS_CSGO___\n");
+	printf("Configuration:_________\n");
+	printf("900 dpi_____1.1 in game\n");
+	printf("Weapons:_______________\n");
+	printf("1.________________AK-47\n");
+	printf("2._________________M4A4\n");
+	printf("$: ");
+}
