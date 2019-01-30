1bfdd35b9b787e33e1fa8d0dcd3ca2bd29754c3a
Henrique Campiotti <henrique260699@hotmail.com>
2019-01-30 12:19:29 -0200

    Update
    -Menu
    -AK47
    -M4A4
    -900dpi
    -1.1 in game

--- src/main.c
+++ src/main.c
@@ -3,28 +3,30 @@
 #include <stdio.h>
 #include "config.h"
 
+#define DEFAULT 15
+#define SELECT	2
+
+#define IAK47 0
+#define IM4A4 1
+
 	void	ExecConfig		(int *dx, int *dy, int *M, int T, int len);
 	void	MouseMove		(int Dx, int Dy, int M, float T);
 	float	CalculateDelay	(int gunTime, int len, float M);
-	void	Menu			();
+	void	Navigation		();
+	void	Switch 			(int index);
+	void	UpdateMenu		();
+
+int M_Index = 0;
+
+bool ak47 = false;
+bool m4a4 = false;
+
+char bools[2][3] = {{"ON"},{"OFF"}};
 
 int main()
 {	
-	int op;
-
-	Menu();
-	scanf("%d", &op);
-	switch(op){
-		case 1:
-			ExecConfig(ak_dx, ak_dy, ak_M, AK47, ak_len);
-		break;
-		case 2:
-			ExecConfig(m4_dx, m4_dy, m4_M, M4A4, m4_len);
-		break;
-		default:
-			printf("Erro!\n");
-	}
-	
+	UpdateMenu();
+	Navigation();
 	return 0;
 }
 
@@ -32,14 +34,12 @@ void ExecConfig(int *dx, int *dy, int *M, int T, int len){
 
 	int i = 0;
 
-	while(true){
-		while(GetAsyncKeyState(VK_LBUTTON) && GetKeyState(VK_HOME)){
-			MouseMove(dx[i], dy[i], M[i], CalculateDelay(T, len, M[i]));
-			i++;
-			if (i == len){break;}
-		}
-		i = 0;
+	while(GetAsyncKeyState(VK_LBUTTON)){
+		MouseMove(dx[i], dy[i], M[i], CalculateDelay(T, len, M[i]));
+		i++;
+		if (i == len){break;}
 	}
+	i = 0;
 }
 
 void MouseMove(int Dx, int Dy, int M, float T){
@@ -59,12 +59,84 @@ float CalculateDelay(int gunTime, int len, float M){
 	return result;
 }
 
-void Menu(){
-	printf("___EXTERNAL_RCS_CSGO___\n");
-	printf("Configuration:_________\n");
-	printf("900 dpi_____1.1 in game\n");
-	printf("Weapons:_______________\n");
-	printf("1.________________AK-47\n");
-	printf("2._________________M4A4\n");
-	printf("$: ");
+void Navigation(){
+
+	while (true){
+
+		if (GetAsyncKeyState(VK_UP)){
+			if (M_Index > 0){
+				M_Index--;
+				UpdateMenu();
+			}
+		}
+		if (GetAsyncKeyState(VK_DOWN)){
+			if (M_Index < 1){
+				M_Index++;
+				UpdateMenu();
+			}
+
+		}
+		if (GetAsyncKeyState(VK_RIGHT)){
+			Switch(M_Index);
+			UpdateMenu();
+		}
+		if (GetAsyncKeyState(VK_LEFT)){
+			Switch(M_Index);
+			UpdateMenu();
+		}
+
+		if (ak47){
+			ExecConfig(ak_dx, ak_dy, ak_M, AK47, ak_len);
+		}
+		if (m4a4){
+			ExecConfig(m4_dx, m4_dy, m4_M, M4A4, m4_len);
+		}
+		
+		Sleep(150);
+	}
+}
+
+void Switch(int index){
+
+	switch(index){
+		case IAK47:
+			ak47 = !ak47;
+			if (m4a4){m4a4 = !m4a4;}
+		break;
+
+		case IM4A4:
+			m4a4 = !m4a4;
+			if (ak47){ak47 = !ak47;}
+		break;
+	}
+
+}
+
+void UpdateMenu(){
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	system("cls");
+
+	SetConsoleTextAttribute(hConsole, DEFAULT);
+
+	printf("________EXTERNAL_RCS________\n");
+	printf("900dpi______________1.1 game\n");
+	printf("____________________________\n");
+
+	if (M_Index == IAK47){
+		SetConsoleTextAttribute(hConsole, SELECT);
+		printf("AK-47\t\t\t[%s]\n", bools[!ak47]);
+	}else{
+		SetConsoleTextAttribute(hConsole, DEFAULT);
+		printf("AK-47\t\t\t[%s]\n", bools[!ak47]);
+	}
+
+	if (M_Index == IM4A4){
+		SetConsoleTextAttribute(hConsole, SELECT);
+		printf("M4A4\t\t\t[%s]\n", bools[!m4a4]);
+	}else{
+		SetConsoleTextAttribute(hConsole, DEFAULT);
+		printf("M4A4\t\t\t[%s]\n", bools[!m4a4]);
+	}
+
 }
